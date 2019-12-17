7dd688d95049a39d16aaf37fbb823b4ac0341822
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2019-12-17 15:28:17 -0600

    updated

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,33 +1,32 @@
-""" Code for our app"""
+"""Code for our app"""
 
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
-# runs with: `set FLASK_app=TWITOFF:APP`
-# and      : `flask run` to run the file
-# or       : `flask shell` to interact in terminal using flask
-    # then : `from TWITOFF.models import *`
-         # : `DB.createall()` to make DB
-         # : `DB` to access it?
-         # : `u1 = User(name = 'name', id = 0)`
-      # or : `u1.name = 'name'
-         # : `t1 = Tweet(text = 'test', id = 0)`
-         # : `DB.session.add(u1)`
-         # : `DB.session.commit()`
-         # : `quit()`
-
-# make our app factory
+#make our app factory
 
 def create_app():
     app = Flask(__name__)
-    
-    # add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-    
-    # have the database know about the app
+
+    #add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    #stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #have the database know about the app
     DB.init_app(app)
-    
+
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!!'
+        users = User.query.all()
+        return render_template('base.html', title = 'Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
+
     return app
